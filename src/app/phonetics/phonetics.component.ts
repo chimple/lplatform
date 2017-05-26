2958fe75785bfdbb7a51a026a19eb1200289d0d7
sharathcb-amiti <sharath.cb@amitisoft.com>
2017-05-26 15:47:08 +0530

    Parent Components created

---
+++ src/app/phonetics/phonetics.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-phonetics',
+  templateUrl: './phonetics.component.html',
+  styleUrls: ['./phonetics.component.css']
+})
+export class PhoneticsComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
