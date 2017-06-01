37fbddc5ad23ccaf598565a288540b3ac51d952f
sharathcb-amiti <sharath.cb@amitisoft.com>
2017-06-01 14:51:40 +0530

    Lang latest

--- src/app/course-detail/lessons/language-word/language-word.component.ts
+++ src/app/course-detail/lessons/language-word/language-word.component.ts
@@ -35,29 +35,11 @@ export class LanguageWordComponent implements OnInit {
     this.course$key = this.route.snapshot.params['lessonId'];
     //this.courid = this.route.snapshot.params['id'];
     console.log(this.lessonWord$Key+"---"+this.course$key+"---"+ this.courid);
-console.log("Sathya id: "+this.route.url);
- this.route.params
-           .subscribe(
-           (params: Params) => {
-           console.log("Latest course id: "+ params);  
-           }
-           )
-           
-
-    /*this.course$key = this.route.snapshot.params.subscribe((params: Params) => {
-<<<<<<< HEAD
-        let paramsURL = params;
-        console.log("Params URL: "+paramsURL);
-      });*/
-      console.log("R1: "+this.route.snapshot.url); // array of states
+    console.log("Sathya id: "+this.route.url);
+    console.log("R1: "+this.route.snapshot.url); // array of states
     console.log("R2: "+this.route.snapshot.url[0].path); 
-
     console.log("Course Key: "+this.course$key);
-=======
-     let paramsURL = params;
-     console.log("Params URL: "+paramsURL);
-     });*/
->>>>>>> refs/remotes/origin/master
+
     this.lesonWord$ = this.lessonService.getLessonItems(this.lessonWord$Key);
     console.log(this.lesonWord$);
   }
