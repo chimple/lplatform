be0ac4bfa6e94a82a85dc0b5e36513c8a6a7c32b
kumar-amiti <kumar@amitisoft.com>
2017-06-01 17:43:53 +0530

    Rechecking top component (due to some conflict)....

--- src/app/topmenu/topmenu.component.ts
+++ src/app/topmenu/topmenu.component.ts
@@ -1,19 +1,24 @@
-import {Component, OnInit} from '@angular/core';
+import {Component, OnInit, ElementRef} from '@angular/core';
 import {AuthService} from '../shared/security/auth.service';
 import {AuthInfo} from '../shared/security/AuthInfo';
 import {Router} from '@angular/router';
 
 @Component({
   selector: 'app-topmenu',
+  host: {'(document:click)': 'handleClick($event)',},
   templateUrl: './topmenu.component.html',
   styleUrls: ['./topmenu.component.css']
 })
 export class TopmenuComponent implements OnInit {
 
   authInfo: AuthInfo;
-  public isCollapsedContent: boolean = false;
-
-  constructor(private authService: AuthService, private router: Router) {
+  public isCollapsedContent:boolean = false;
+  public isCollapsedCourses:boolean = false;
+  public elementRef;
+ 
+    
+  constructor(private authService: AuthService, private router: Router,private myElement: ElementRef) {
+    this.elementRef = myElement;
   }
 
   ngOnInit() {
@@ -21,21 +26,44 @@ export class TopmenuComponent implements OnInit {
       .subscribe(
         authInfo => {
           this.authInfo = authInfo
-          console.log(this.authInfo.getUser());
+          console.log(this.authInfo.getUser())
         }
       );
+  }
+
+  handleClick(event){
+    var clickedComponent = event.target;
+    var inside = false;
+    do{
+        if (clickedComponent === this.elementRef.nativeElement) {
+           inside = true;
+        }
+        clickedComponent = clickedComponent.parentNode;
+    } while (clickedComponent);
+    if(!inside){
+       this.isCollapsedContent = false;
+      this.isCollapsedCourses = false;
+    }
   }
 
   logout() {
     this.authService.logout();
     this.router.navigate(['/']);
   }
-
-  showUserInfo() {
-    if (this.isCollapsedContent === false) {
+  showUserInfo(){
+    if(this.isCollapsedContent == false){
       this.isCollapsedContent = true;
-    } else if (this.isCollapsedContent === true) {
+      this.isCollapsedCourses = false;
+    }else if(this.isCollapsedContent == true){
       this.isCollapsedContent = false;
+    }
+  }
+  showUserCourses(){
+    if(this.isCollapsedCourses == false){
+      this.isCollapsedCourses = true;
+      this.isCollapsedContent = false;
+    }else if(this.isCollapsedCourses == true){
+      this.isCollapsedCourses = false;
     }
   }
 }
