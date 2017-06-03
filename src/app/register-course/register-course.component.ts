b231f191aada8804cb2587cc9400541709e5e275
sarath-amiti <sarath@amitisoft.com>
2017-06-03 14:10:29 +0530

    Register Course with Static UID

--- src/app/register-course/register-course.component.ts
+++ src/app/register-course/register-course.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import {NgForm} from '@angular/forms';
 
 @Component({
   selector: 'app-register-course',
@@ -7,9 +8,13 @@ import { Component, OnInit } from '@angular/core';
 })
 export class RegisterCourseComponent implements OnInit {
 
-  constructor() { }
+  constructor( ) { }
 
   ngOnInit() {
+  }
+
+  courseReg(registerForm: NgForm) {
+    console.log(registerForm.value);
   }
 
 }
